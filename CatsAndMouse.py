c8369e43216ede6a18819b6328da45650b37dc5a
Sudip Bhunia <80640681+SudipBhunia@users.noreply.github.com>
2024-10-15 00:55:04 +0530

    Create CatsAndMouse.py

---
+++ CatsAndMouse.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the catAndMouse function below.
+def catAndMouse(x, y, z):
+    if abs(x-z) < abs(y-z):
+        res = 'Cat A'
+        return res
+    elif abs(x-z) > abs(y-z):
+        res1 = 'Cat B'
+        return res1
+    else:
+        res3 = 'Mouse C'
+        return res3
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        xyz = input().split()
+
+        x = int(xyz[0])
+
+        y = int(xyz[1])
+
+        z = int(xyz[2])
+
+        result = catAndMouse(x, y, z)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
